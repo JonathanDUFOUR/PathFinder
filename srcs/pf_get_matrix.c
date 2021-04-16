3d1a138a72e03b74b7ef64cede17b923dba2d9cf
jodufour <jodufour@student.42.fr>
2021-04-16 20:41:49 +0200

    Norminetted the debugging

--- srcs/pf_get_matrix.c
+++ srcs/pf_get_matrix.c
@@ -6,12 +6,26 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 21:20:23 by jodufour          #+#    #+#             */
-/*   Updated: 2021/04/16 15:26:34 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/04/16 16:19:19 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "pathfinder.h"
+
+static void	pf_calc_s_e_values(int *matrix, uint32_t dim[2], uint32_t idx[3])
+{
+	matrix[idx[I_START]] = pf_calc_dist(dim, idx[I_START], idx[I_END]);
+	matrix[idx[I_END]] = pf_calc_dist(dim, idx[I_START], idx[I_END]);
+}
+
+static int	*pf_malloc_matrix(int **matrix, uint32_t dim[2])
+{
+	*matrix = malloc (dim[H] * dim[W] * sizeof(int));
+	if (!(*matrix))
+		return (NULL);
+	return (*matrix);
+}
 
 int	*pf_get_matrix(char *map, uint32_t dim[2], uint32_t idx[3])
 {
@@ -19,8 +33,7 @@ int	*pf_get_matrix(char *map, uint32_t dim[2], uint32_t idx[3])
 	uint32_t	i;
 	uint32_t	size;
 
-	matrix = malloc (dim[H] * dim[W] * sizeof(int));
-	if (!matrix)
+	if (!(pf_malloc_matrix(&matrix, dim)))
 		return (NULL);
 	i = 0;
 	size = dim[H] * dim[W];
@@ -39,7 +52,6 @@ int	*pf_get_matrix(char *map, uint32_t dim[2], uint32_t idx[3])
 		}
 		++i;
 	}
-	matrix[idx[I_START]] = pf_calc_dist(dim, idx[I_START], idx[I_END]);
-	matrix[idx[I_END]] = pf_calc_dist(dim, idx[I_START], idx[I_END]);
+	pf_calc_s_e_values(matrix, dim, idx);
 	return (matrix);
 }
