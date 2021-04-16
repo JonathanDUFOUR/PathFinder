27a127bf55b4c40dddd8be3017d6f90d2de858fe
jodufour <jodufour@student.42.fr>
2021-04-16 09:38:59 +0200

    Added colors to the print of the matrix, continue debugging

--- srcs/pf_print_matrix.c
+++ srcs/pf_print_matrix.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 21:36:38 by jodufour          #+#    #+#             */
-/*   Updated: 2021/04/15 22:48:42 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/04/16 09:36:23 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	pf_print_matrix(int *matrix, uint32_t dim[2])
 {
 	uint32_t	i;
 	uint32_t	j;
+	int			cell;
 
 	i = 0;
 	while (i < dim[H])
@@ -24,7 +25,23 @@ void	pf_print_matrix(int *matrix, uint32_t dim[2])
 		j = 0;
 		while (j < dim[W])
 		{
-			printw("% *d ", 3, matrix[i * dim[W] + j]);
+			cell = matrix[i * dim[W] + j];
+			if (cell == M_EMPTY)
+				attron(COLOR_PAIR(EMPTY_COLOR));
+			else if (cell == M_START)
+				attron(COLOR_PAIR(START_COLOR));
+			else if (cell == M_END)
+				attron(COLOR_PAIR(END_COLOR));
+			else if (cell == M_OBSTACLE)
+				attron(COLOR_PAIR(OBSTACLE_COLOR));
+			else
+				attron(COLOR_PAIR(PATH_COLOR));
+			printw("% *d", 3, cell);
+			attroff(COLOR_PAIR(EMPTY_COLOR));
+			attroff(COLOR_PAIR(START_COLOR));
+			attroff(COLOR_PAIR(END_COLOR));
+			attroff(COLOR_PAIR(OBSTACLE_COLOR));
+			attroff(COLOR_PAIR(PATH_COLOR));
 			++j;
 		}
 		printw("\n");
--- srcs/pf_setup.c
+++ srcs/pf_setup.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 08:56:44 by jodufour          #+#    #+#             */
-/*   Updated: 2021/04/14 19:47:12 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/04/16 09:21:44 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 void	pf_setup(void)
 {
 	start_color();
-	init_pair(START_COLOR, COLOR_YELLOW, COLOR_YELLOW);
-	init_pair(END_COLOR, COLOR_GREEN, COLOR_GREEN);
-	init_pair(OBSTACLE_COLOR, COLOR_WHITE, COLOR_WHITE);
-	init_pair(EMPTY_COLOR, COLOR_BLACK, COLOR_BLACK);
-	init_pair(PATH_COLOR, COLOR_RED, COLOR_BLACK);
-	init_pair(FIND_COLOR, COLOR_CYAN, COLOR_BLACK);
+	init_pair(START_COLOR, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(END_COLOR, COLOR_BLACK, COLOR_GREEN);
+	init_pair(OBSTACLE_COLOR, COLOR_BLACK, COLOR_WHITE);
+	init_pair(EMPTY_COLOR, COLOR_WHITE, COLOR_BLACK);
+	init_pair(PATH_COLOR, COLOR_BLACK, COLOR_RED);
+	init_pair(FIND_COLOR, COLOR_BLACK, COLOR_CYAN);
 	init_pair(ERR_MSG_COLOR, COLOR_RED, COLOR_BLACK);
 	curs_set(0);
 	cbreak();
